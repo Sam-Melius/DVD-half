9c569810c159576e80c7a7f4d0f8fb4582469de9
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-03-21 12:02:21 -0700

    exercise 1 passing

--- exercise_1/exercise-1.sql
+++ exercise_1/exercise-1.sql
@@ -1,2 +1,13 @@
 -- find all address_id of addresses in the city of Dundee
 -- use INNER JOIN FROM address on to city
+SELECT
+    address.address_id
+FROM
+    address
+INNER JOIN
+    city
+ON
+    city.city_id = address.city_id
+WHERE
+	address.address_id = 146
+
